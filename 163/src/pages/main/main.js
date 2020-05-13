57323f4b5069b5373a344e6a29747382098b1c8b
imouou <imouou@gmail.com>
2020-05-13 22:42:39 +0800

    工程素材

---
+++ 163/src/pages/main/main.js
@@ -0,0 +1,20 @@
+/**
+ * 首页模块
+ * 默认模块名: main
+ * @return {[object]}  [ 返回一个对象 ]
+ */
+loader.define(function(require, exports, module) {
+
+    var pageview = {};
+
+    // 模块初始化定义
+    pageview.init = function() {
+
+    }
+
+    // 初始化
+    pageview.init();
+
+    // 输出模块
+    return pageview;
+})
