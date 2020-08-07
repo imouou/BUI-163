c9d0bef5ec0e46b5f0defcafecadea51b39c4530
imouou <imouou@gmail.com>
2020-08-07 10:30:38 +0800

    163

--- 163/src/index.js
+++ 163/src/index.js
@@ -2,50 +2,50 @@
  打包需要设置isWebapp为false 才能绑定后退按键
 ------------------------------------------- */
 
-bui.isWebapp = false;
+bui.isWebapp = true;
 // 原生方法
-bui.on("pageready",function(){
-  // 绑定手机后退按键
-  bindBack();
+bui.on("pageready", function() {
+    // 绑定手机后退按键
+    bindBack();
 });
 
 // 绑定手机后退按键
 function bindBack() {
-// 监听后退按钮
-var flag = 0,
-	quickClick = 0,
-	timeout;
-try{
-  // 隐藏滚动条
-  plus.webview.currentWebview().setStyle({scrollIndicator:'none'});
+    // 监听后退按钮
+    var flag = 0,
+        quickClick = 0,
+        timeout;
+    try {
+        // 隐藏滚动条
+        plus.webview.currentWebview().setStyle({ scrollIndicator: 'none' });
 
-   plus.key.addEventListener('backbutton', function(){
-	if( router.getHistory().length > 1 ){
-		// 防止快速点击导致历史记录错乱
-	  if( quickClick == 0 ){
-		bui.back();
-		quickClick = 1;
-		//.5s后重新设置回去
-		timeout = setTimeout(function() {
-		  quickClick = 0;
-		 }, 500);
-	  }
-	}else{
-		if (flag == 0) {
-		 bui.hint("再按一次就退出应用了!");
-		 flag = flag + 1;
-		 //2s后重新设置回去
-		 timeout = setTimeout(function() {
-		  flag = 0;
-		 }, 2000);
-		} else if (flag == 1) {
-		  plus.runtime.quit();
+        plus.key.addEventListener('backbutton', function() {
+            if (router.getHistory().length > 1) {
+                // 防止快速点击导致历史记录错乱
+                if (quickClick == 0) {
+                    bui.back();
+                    quickClick = 1;
+                    //.5s后重新设置回去
+                    timeout = setTimeout(function() {
+                        quickClick = 0;
+                    }, 500);
+                }
+            } else {
+                if (flag == 0) {
+                    bui.hint("再按一次就退出应用了!");
+                    flag = flag + 1;
+                    //2s后重新设置回去
+                    timeout = setTimeout(function() {
+                        flag = 0;
+                    }, 2000);
+                } else if (flag == 1) {
+                    plus.runtime.quit();
 
-		}
-	}
+                }
+            }
 
-   }, false);
-}catch(e){}
+        }, false);
+    } catch (e) {}
 }
 
 
